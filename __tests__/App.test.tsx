d576decc3e8e1b207f56623ef94ba41184b13c8f
sitogi <takano162@gmail.com>
2022-06-30 21:35:54 +0900

    Use abs path from base url in import statements

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from '../src/App';
+import App from '~/App';
 
 test('render App component', () => {
   render(<App />);
