467733f8ccfd2aee04be7902a41f67e848258690
sitogi <takano162@gmail.com>
2023-02-09 12:35:45 +0900

    chore: Add Stylelint and remove some files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import '~/index.css';
+import '~/main.css';
 import App from '~/App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
