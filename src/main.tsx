3a7c3f0cd83db905f902611ba7f3aa60f70131dd
sitogi <takano162@gmail.com>
2023-02-10 22:08:48 +0900

    chore: use npm instead of yarn

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '~/main.css';
-import App from '~/App';
+import { App } from '~/App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
