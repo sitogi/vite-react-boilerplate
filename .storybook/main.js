3a1a48d98bcec3184b957d7de2f6919baf16b8de
sitogi <takano162@gmail.com>
2023-02-10 22:18:49 +0900

    chore: add dark mode plugin of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
