e1242b3d1c3c0c75c0f612347f25664fe889b7d4
sitogi <takano162@gmail.com>
2023-04-27 21:33:41 +0900

    chore: add mdx for App.stories.tsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,28 @@
 const path = require('path');
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-dark-mode', '@storybook/addon-mdx-gfm'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/react-vite',
-    options: {}
+    options: {},
   },
   features: {
-    storyStoreV7: true
+    storyStoreV7: true,
   },
-  viteFinal: async config => {
+  viteFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '~': path.resolve(__dirname, '../src')
+      '~': path.resolve(__dirname, '../src'),
     };
     return config;
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
