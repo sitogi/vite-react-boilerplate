d557c586d86dfa967d0c13708a52b520d29227e3
sitogi <takano162@gmail.com>
2025-02-07 00:55:35 +0900

    make test works

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
-
-import { resolve } from './vite.config';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
   },
-  resolve: resolve,
 });
